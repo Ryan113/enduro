733abc525e9c2ddabeb903b21fe9d289ac53cc60
Ryan <>
2018-10-05 18:29:15 -0700

    init

---
+++ test/cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: 'd0da7eea36bcc6b6ea7cf1941c5773d3',
+			hash: 'd7630e9b9ccf19d39e9c05c613d8a6891b190e21e5cd6c9416006fb0bb9f5c4c',
+			user_created_timestamp: 1538788755169
+		}
+	]
+})
---
+++ test/cms/index.js
@@ -0,0 +1,82 @@
+({
+	name: 'Vacation GetAway',
+	specialty: 'Cute pictures',
+	$main_image_type: 'image',
+	main_image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880097_juliesmith_low.jpg',
+	$background_type: 'textarea',
+	background: 'Cute home tucked away where happiness is never short. We aim to make sure you have the coziest place ever. <3',
+	$projects_min: 1,
+	projects: [
+		{
+			title: 'Full Kitchen',
+			$image_type: 'image',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880340_pexels-photo-27631.jpg',
+			link: 'http://newatlas.com/boeing-echo-voyager/42272/'
+		},
+		{
+			title: 'Game room',
+			$image_type: 'image',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880379_pexels-photo.jpg',
+			link: 'https://www.technologyreview.com/s/537366/the-machine-vision-algorithm-beating-art-historians-at-their-own-game/'
+		},
+		{
+			title: 'Hot tub',
+			$image_type: 'image',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880400_pexels-photo-301871.jpeg',
+			link: 'http://www.neurala.com/drone-logistics-security/'
+		},
+		{
+			title: 'Living room',
+			$image_type: 'image',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880412_pexels-photo-132987.jpeg',
+			link: 'https://medium.com/@ageitgey/machine-learning-is-fun-part-3-deep-learning-and-convolutional-neural-networks-f40359318721#.otc8klxv3'
+		},
+		{
+			title: 'Large dinnning',
+			$image_type: 'image',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880425_pexels-photo-57825.jpeg',
+			link: 'https://www.meetup.com/The-Deep-Learning-Group/'
+		},
+		{
+			title: 'Cute stuff',
+			$image_type: 'image',
+			image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880436_pexels-photo-279607.jpeg',
+			link: 'https://www.cs.cmu.edu/news/carnegie-mellon-project-uses-machine-learning-help-apartment-hunters-estimate-utility-costs'
+		}
+	],
+	$social_max: 3,
+	$social_min: 1,
+	social: [
+		{
+			$icon_type: 'fa',
+			icon: 'instagram',
+			text: 'Check out our instagram!',
+			link: 'https://github.com/Gottwik/Enduro'
+		}
+	],
+	$hobby_background_type: 'image',
+	hobby_background: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880478_pexels-photo-92323.jpeg',
+	hobby_title: 'Nook room for good times',
+	hobby_subtitle: 'Books are the old-school internet',
+	cta_tagline: 'Need a great vacation?',
+	cta_text: '',
+	contact_email: 'contact@endurojs.com',
+	footer_text: '2017 @enduro.js mirror theme',
+	$footer_background_color_type: 'colorpicker',
+	footer_background_color: '#7e0343',
+	page_title: 'Mirror - enduro.js theme',
+	display_edit_stamp: true,
+	$style_expander: true,
+	$style_info: 'won\'t work with the temp action',
+	style: {
+		$background_color_size: 'inline',
+		$background_color_type: 'colorpicker',
+		background_color: '#ffffff',
+		$text_color_size: 'inline',
+		$text_color_type: 'colorpicker',
+		text_color: '#2b2928',
+		$accent_color_size: 'inline',
+		$accent_color_type: 'colorpicker',
+		accent_color: '#fafdff'
+	}
+})
